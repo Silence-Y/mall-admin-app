066a337dcc4efce5ae5a742358e45340130dd039
Slience-Y <maqiyan9@gmail.com>
2021-02-28 15:33:42 +0800

    update

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,9 @@
 import axios from 'axios';
+import store from './store'
+import {
+  config
+} from 'shelljs';
+
 //创建一个axios实例
 const instance = axios.create({
   // 配置信息,
@@ -6,13 +11,26 @@ const instance = axios.create({
 });
 // 请求拦截器
 instance.interceptors.request.use((config) => {
-  console.log(config);
-  return config;
+  // console.log(config);
+
+  // 请求拦截前加入appkey
+  // 给商品列表中的请求参数中加入appkey
+  if (config.url.includes('/passport')) {
+    return config
+  } else {
+    return {
+      ...config,
+      params: {
+        ...config.params,
+        appkey: store.state.user.appkey
+      }
+    }
+  }
 }, (error) => Promise.reject(error));
 
 // 响应拦截器
 instance.interceptors.response.use((response) => {
-  console.log(response);
+  // console.log(response);
   if (response.data.status === 'fail') {
     return Promise.reject(response.data.msg);
   }
--- src/views/page/productList.vue
+++ src/views/page/productList.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>fsdf</div>
+  <div class="product-list">
+    <!-- 搜素框 -->
+    <search-box @submit="searchSubmit" />
+
+    <!-- 表格 -->
+    <productTable />
+  </div>
 </template>
 
 <script>
-export default {};
+import searchBox from "@/components/search.vue";
+import productTable from "@/components/productsTable.vue";
+// import api from "@/api/product";
+// import categoryApi from "@/api/category";
+export default {
+  data() {
+    return {
+      searchForm: {}
+    };
+  },
+  components: {
+    searchBox,
+    productTable
+  },
+  async created() {},
+  methods: {
+    searchSubmit(form) {
+      console.log(form);
+      this.searchForm = form;
+    }
+  }
+};
 </script>
